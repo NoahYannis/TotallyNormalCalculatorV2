8e2bd6ee4ea03fdaed8963b07cc5b0576dc80e03
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2024-06-02 10:28:46 +0200

    expanding logger + debug logging

--- TotallyNormalCalculator/Core/Helper.cs
+++ TotallyNormalCalculator/Core/Helper.cs
@@ -1,11 +1,31 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using TotallyNormalCalculator.Logging;
 
 namespace TotallyNormalCalculator.Core;
 
 public static class Helper
 {
+    private static readonly TotallyNormalCalculatorLogger _logger = new();
     public static string GetConnectionString(string name)
     {
-        return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        try
+        {
+
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+        catch (Exception exc)
+        {
+            // Debugging
+            _logger.LogMessageToTempFile($"ConfigurationManager.ConnectionStrings.Count: {ConfigurationManager.ConnectionStrings.Count}" +
+                $"\nConfigurationManager.ConnectionStrings[0]: {ConfigurationManager.ConnectionStrings[0]}" +
+                $"\nConnection string name: {name}" +
+                $"\nConfigurationManager.ConnectionStrings[name] is null: {ConfigurationManager.ConnectionStrings[name] is null}" +
+                $"\nConfigurationManager.ConnectionStrings[name]?.ConnectionString is null: {ConfigurationManager.ConnectionStrings[name]?.ConnectionString is null}");
+           
+            _logger.LogExceptionToTempFile(exc);
+        }
+
+        return string.Empty;
     }
 }
--- TotallyNormalCalculator/Logging/TotallyNormalCalculatorLogger.cs
+++ TotallyNormalCalculator/Logging/TotallyNormalCalculatorLogger.cs
@@ -5,18 +5,29 @@ using System.Windows;
 namespace TotallyNormalCalculator.Logging;
 public class TotallyNormalCalculatorLogger : ILogger
 {
+    private static readonly string logFolderPath = Path.Combine(Path.GetTempPath(), "TotallyNormalCalculator_Logs");
+    private static readonly string logFilePath = Path.Combine(logFolderPath, "tnc-exception.log");
+
     public void LogExceptionToTempFile(Exception exc)
     {
-        string logFolderPath = Path.Combine(Path.GetTempPath(), "TotallyNormalCalculator_Logs");
-        string logFilePath = Path.Combine(logFolderPath, "tnc-exception.log");
 
         string logMessage =
-            $"{DateTime.Now:dd.MM.yyyy HH:mm:ss}" +
-            $"\n Exception: {exc.GetType()} " +
-            $"\n Error Message: {exc.Message}" +
-            $"\n Stack Trace: {exc.StackTrace} " +
-            $"\n\n";
+                $"{DateTime.Now:dd.MM.yyyy HH:mm:ss}" +
+                $"\n Exception: {exc.GetType()} " +
+                $"\n Error Message: {exc.Message}" +
+                $"\n Stack Trace: {exc.StackTrace} " +
+                $"\n\n";
 
+        WriteToFile(logMessage);
+    }
+
+    public void LogMessageToTempFile(string logMessage)
+    {
+        WriteToFile(logMessage);
+    }
+
+    private void WriteToFile(string logMessage)
+    {
         try
         {
             if (!Directory.Exists(logFolderPath))
