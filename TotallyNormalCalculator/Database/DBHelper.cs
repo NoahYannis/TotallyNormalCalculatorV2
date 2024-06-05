09003685f1585ddfe2fa60a78a3581ef84994c50
NoahYannis <noah.guderjahn@gmx.de>
2024-06-05 19:42:59 +0200

    Loading the database creation scripts as embedded resources

--- TotallyNormalCalculator/Database/DBHelper.cs
+++ TotallyNormalCalculator/Database/DBHelper.cs
@@ -2,6 +2,7 @@
 using System.Configuration;
 using System.Data.SqlClient;
 using System.IO;
+using System.Reflection;
 using TotallyNormalCalculator.Logging;
 
 namespace TotallyNormalCalculator.Core;
@@ -42,6 +43,7 @@ public static class DBHelper
 
             if (!CheckIfAppDbExists(masterConnectionString))
             {
+                _logger.LogMessageToTempFile("Database does not exist. Creating database...");
                 CreateDatabase(masterConnectionString);
             }
         }
@@ -69,13 +71,10 @@ public static class DBHelper
 
     private static void CreateDatabase(string connectionString)
     {
-        string createDBScriptPath = "Database\\CreateDiaryEntryDB_Script.sql";
-        string createTableScriptPath = "Database\\CreateEntriesTable_Script.sql";
-
         try
         {
-            string createDBScript = File.ReadAllText(createDBScriptPath);
-            string createTableScript = File.ReadAllText(createTableScriptPath);
+            string createDBScript = LoadEmbeddedResource("TotallyNormalCalculator.Database.CreateDiaryEntryDB_Script.sql");
+            string createTableScript = LoadEmbeddedResource("TotallyNormalCalculator.Database.CreateEntriesTable_Script.sql");
 
             using (var connection = new SqlConnection(connectionString))
             {
@@ -105,4 +104,37 @@ public static class DBHelper
             _logger.LogExceptionToTempFile(exc);
         }
     }
+
+    /// <summary>
+    /// Returns the read script as a string.
+    /// </summary>
+    /// <param name="resourceName">The name of the embedded resource.</param>
+    /// <returns>The content of the embedded resource as a string.</returns>
+    /// <exception cref="FileNotFoundException">Thrown when the resource is not found.</exception>
+    private static string LoadEmbeddedResource(string resourceName)
+    {
+        try
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                {
+                    throw new FileNotFoundException();
+                }
+
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            _logger.LogMessageToTempFile($"The embedded resource {resourceName} was not found: {ex.Message}");
+        }
+
+        return string.Empty;
+    }
+
 }
