af1d7ca3348a6c4a2f70848b2058f7bb44e83ac3
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2024-06-16 09:09:53 +0200

    diaryview cleanup

--- TotallyNormalCalculator/MVVM/Model/DiaryEntryModel.cs
+++ TotallyNormalCalculator/MVVM/Model/DiaryEntryModel.cs
@@ -4,9 +4,8 @@ namespace TotallyNormalCalculator.MVVM.Model;
 
 public class DiaryEntryModel : ObservableObject
 {
-    public int Id { get; set; }
+    public int Id { get; private set; }
     public string Title { get; set; }
     public string Message { get; set; }
     public string Date { get; set; }
-
 }
