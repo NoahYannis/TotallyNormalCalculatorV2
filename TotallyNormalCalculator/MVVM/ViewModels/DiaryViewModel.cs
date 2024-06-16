ab7e9af0aeaf575f72e788c6b9291789ab174072
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2024-06-16 15:38:46 +0200

    implemented filter for diary date and message including placeholder text

--- TotallyNormalCalculator/MVVM/ViewModels/DiaryViewModel.cs
+++ TotallyNormalCalculator/MVVM/ViewModels/DiaryViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
@@ -29,6 +30,9 @@ public partial class DiaryViewModel : BaseViewModel
 
     [ObservableProperty]
     private ObservableCollection<DiaryEntryModel> _filteredEntries = new();
+
+    [ObservableProperty]
+    private string _propertyFilter;
 
     [ObservableProperty]
     private DiaryEntryModel _selectedEntry;
@@ -146,10 +150,25 @@ public partial class DiaryViewModel : BaseViewModel
     [RelayCommand]
     private void FilterEntries(string text)
     {
-        FilteredEntries = new ObservableCollection<DiaryEntryModel>
-            (_entries.Where(e => e.Title.Contains(text)));
+        FilteredEntries = PropertyFilter switch
+        {
+            "Title" => FilterEntries(e => e.Title != null && e.Title.Contains(text)),
+            "Message" => FilterEntries(e => e.Message != null && e.Message.Contains(text)),
+            "Date" => FilterEntries(e => e.Date != null && e.Date.Contains(text)),
+            _ => []
+        };
 
         OnPropertyChanged(nameof(Entries));
+    }
+
+    /// <summary>
+    /// Takes all diary entries and returns those that satisfy the condition.
+    /// </summary>
+    /// <param name="filterCondition"></param>
+    /// <returns></returns>
+    private ObservableCollection<DiaryEntryModel> FilterEntries(Func<DiaryEntryModel, bool> filterCondition)
+    {
+        return new ObservableCollection<DiaryEntryModel>(_entries.Where(filterCondition));
     }
 
 
@@ -178,6 +197,11 @@ public partial class DiaryViewModel : BaseViewModel
 
     partial void OnFilterTextChanged(string value)
     {
+        if (PropertyFilter is null)
+        {
+            return;
+        }
+
         FilterEntries(value);
     }
 
