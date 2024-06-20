aab907775ef07e9da48f1af67d13bba4952f96ca
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2024-06-20 11:44:18 +0200

    Styles for disabled buttons + created a correspondig converter

---
+++ TotallyNormalCalculator/Converter/SelectionToEnabledConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using TotallyNormalCalculator.MVVM.Model;
+
+namespace TotallyNormalCalculator.Converter;
+
+public class SelectionToEnabledConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is DiaryEntryModel;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
