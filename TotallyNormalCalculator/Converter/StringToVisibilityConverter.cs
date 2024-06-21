78e5781e293af75a7a4b0b588b994da4dc9145f7
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2024-06-21 22:11:49 +0200

    Refactoring

---
+++ TotallyNormalCalculator/Converter/StringToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using TotallyNormalCalculator.MVVM.Model;
+
+namespace TotallyNormalCalculator.Converter;
+
+public class StringToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is string str && string.IsNullOrEmpty(str) ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
