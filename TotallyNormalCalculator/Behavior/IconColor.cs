44ba29d1f09a2a35a294c1adc750ad58878dbb7d
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2024-06-24 18:54:10 +0200

    Added an attached property to set the icon color on ui elements

---
+++ TotallyNormalCalculator/Behavior/IconColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace TotallyNormalCalculator.Behavior;
+
+/// <summary>
+/// An attached property to set the font awesome icon color on controls
+/// </summary>
+public static class IconColor
+{
+    public static readonly DependencyProperty IconColorProperty =
+        DependencyProperty.RegisterAttached("IconColor", typeof(string), typeof(IconColor), new PropertyMetadata("Black"));
+
+    public static void SetIconColor(UIElement element, string value)
+    {
+        element.SetValue(IconColorProperty, value);
+    }
+
+    public static string GetIconColor(UIElement element)
+    {
+        return (string)element.GetValue(IconColorProperty);
+    }
+}
