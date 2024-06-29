4c6548cc6e39a6165723df795ad70a53f1fabeb0
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2024-06-29 10:01:00 +0200

    Displaying a loading spinner while blobs are fetched

---
+++ TotallyNormalCalculator/Converter/NullToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TotallyNormalCalculator.Converter;
+
+public class NullToVisibilityConverter : IValueConverter
+{
+    public bool NullEqualsVisible { get; set; }
+
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        bool isVisible = value is not null;
+
+        if (NullEqualsVisible)
+        {
+            isVisible = !isVisible;
+        }
+
+        return isVisible ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
