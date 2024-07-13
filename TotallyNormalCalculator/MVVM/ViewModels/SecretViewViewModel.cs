ab6c321dc48419b7dc9885420e51c9e28c223065
Noah Yannis Guderjahn <noah.guderjahn@gmx.de>
2024-07-13 18:33:57 +0200

    theme can now only be changed from settings tab

--- TotallyNormalCalculator/MVVM/ViewModels/SecretViewViewModel.cs
+++ TotallyNormalCalculator/MVVM/ViewModels/SecretViewViewModel.cs
@@ -1,32 +1,13 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Windows;
 
 namespace TotallyNormalCalculator.MVVM.ViewModels;
 internal partial class SecretViewViewModel : BaseViewModel
 {
-    [ObservableProperty]
-    private bool _isLightTheme = true;
-
     public SecretViewViewModel()
     {
         SelectedViewModel = App.AppHost.Services.GetRequiredService<DiaryViewModel>();
-    }
-
-
-    [RelayCommand]
-    public void SwitchTheme()
-    {
-        IsLightTheme = !IsLightTheme;
-
-        string newThemePath = IsLightTheme ? "Themes/LightTheme.xaml" : "Themes/DarkTheme.xaml";
-        string oldThemePath = IsLightTheme ? "Themes/DarkTheme.xaml" : "Themes/LightTheme.xaml";
-        var newTheme = (ResourceDictionary)Application.LoadComponent(new Uri(newThemePath, UriKind.Relative));
-        var oldTheme = (ResourceDictionary)Application.LoadComponent(new Uri(oldThemePath, UriKind.Relative));
-        Application.Current.Resources.MergedDictionaries.Remove(oldTheme);
-        Application.Current.Resources.MergedDictionaries.Add(newTheme);
     }
 
     [RelayCommand]
